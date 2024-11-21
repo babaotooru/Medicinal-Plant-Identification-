05ac5d7ee165ecdf67103dab79049516ae30db87
babaotooru <104141240+babaotooru@users.noreply.github.com>
2024-11-21 16:08:13 +0530

    code

---
+++ main.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+import base64
+import os
+
+model = load_model('Model/model.h5')
+class_dict = np.load("artifacts/class_names.npy")
+
+
+def predict(image):
+    IMG_SIZE = (1, 224, 224, 3)
+
+    img = image.resize(IMG_SIZE[1:-1])
+    img_arr = np.array(img)
+    img_arr = img_arr.reshape(IMG_SIZE)
+
+    pred_proba = model.predict(img_arr)
+    pred = np.argmax(pred_proba)
+    return pred
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+contnt = "<p>Herbal medicines are preferred in both developing and developed countries as an alternative to " \
+         "synthetic drugs mainly because of no side effects. Recognition of these plants by human sight will be " \
+         "tedious, time-consuming, and inaccurate.</p> " \
+         "<p>Applications of image processing and computer vision " \
+         "techniques for the identification of the medicinal plants are very crucial as many of them are under " \
+         "extinction as per the IUCN records. Hence, the digitization of useful medicinal plants is crucial " \
+         "for the conservation of biodiversity.</p>"
+
+if __name__ == '__main__':
+    add_bg_from_local("artifacts/Background.jpg")
+    new_title = '<p style="font-family:sans-serif; color:White; font-size: 42px;">Medicinal Leaf Classification</p>'
+    st.markdown(new_title, unsafe_allow_html=True)
+    st.markdown(contnt, unsafe_allow_html=True)
+    uploaded_file = st.file_uploader("Choose a file")
+
+    if uploaded_file is not None:
+        img = Image.open(uploaded_file)
+
+        img = img.resize((300, 300))
+        st.image(img)
+
+        if st.button("Predict"):
+            pred = predict(img)
+            name = class_dict[pred]
+        p=r"C:\\Users\\babao\\Documents\\one drive\\OneDrive\Desktop\\data\\data"    
+        if os.path.isdir(p):
+            for file in os.listdir(p):
+                file_name, file_ext = os.path.splitext(file)
+                if file_name.lower() == name.lower() and file_ext == '.txt':
+                    matched_file = os.path.join(p, file)
+                    break
+
+        if matched_file:
+            with open(matched_file, 'r') as file:
+                file_content = file.read()
+            result = f'<p style="font-family:sans-serif; color:White; font-size: 16px;">' \
+                    f'The given image is {name}.<br>Details:<br>{file_content}</p>'
+        else:
+            result = f'<p style="font-family:sans-serif; color:White; font-size: 16px;">' \
+                    f'The given image is {name}, but no matching file was found in the directory.</p>'
+
+        st.markdown(result, unsafe_allow_html=True)
+
+
+
